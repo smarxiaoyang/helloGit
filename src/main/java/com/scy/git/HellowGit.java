dd66a6415e333acaa9ac5f64cf2d8279eb497732
dell <a15132121872@163.com>
2024-04-16 11:05:11 +0800

    创建分支

--- src/main/java/com/scy/git/HellowGit.java
+++ src/main/java/com/scy/git/HellowGit.java
@@ -4,5 +4,6 @@ public class HellowGit {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello 1");
+        System.out.println("创建分支");
     }
 }
