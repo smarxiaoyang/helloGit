a63c96a27e251acc34c4c0900749857262e4690e
dell <a15132121872@163.com>
2024-04-16 10:52:47 +0800

    更新1

--- src/main/java/com/scy/git/HellowGit.java
+++ src/main/java/com/scy/git/HellowGit.java
@@ -3,5 +3,6 @@ package com.scy.git;
 public class HellowGit {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello 1");
     }
 }
